1e4b8c38ed2c07776dcc2d5bd1f03e692253d3af
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2017-11-27 18:07:04 -0300

    antes de modal

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,24 @@
 class EventsController < ApplicationController
-  
-  before_action :set_event, only: [:show]
-  
+
+  before_action :set_event, only: [:show, :update]
+
   def index
   	@events = Event.all
   end
-  
+
   def show
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
